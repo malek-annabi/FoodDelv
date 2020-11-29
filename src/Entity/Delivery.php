e3f3b7de902896019496b9b4949f7bc8fb8e6144
malek <malekannabi5@gmail.com>
2020-11-29 13:36:43 +0100

    init

--- src/Entity/Delivery.php
+++ src/Entity/Delivery.php
@@ -17,8 +17,111 @@ class Delivery
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $Wallet = [];
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Location;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $Time;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Price;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $Status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $User;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getWallet(): ?array
+    {
+        return $this->Wallet;
+    }
+
+    public function setWallet(array $Wallet): self
+    {
+        $this->Wallet = $Wallet;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->Location;
+    }
+
+    public function setLocation(string $Location): self
+    {
+        $this->Location = $Location;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->Time;
+    }
+
+    public function setTime(?\DateTimeInterface $Time): self
+    {
+        $this->Time = $Time;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(float $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->Status;
+    }
+
+    public function setStatus(string $Status): self
+    {
+        $this->Status = $Status;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): self
+    {
+        $this->User = $User;
+
+        return $this;
     }
 }
--- src/Entity/Food.php
+++ src/Entity/Food.php
@@ -17,8 +17,59 @@ class Food
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $ingredients = [];
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Price;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(float $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
     }
 }
