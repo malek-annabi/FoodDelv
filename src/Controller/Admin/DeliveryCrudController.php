28b5122c560669a32b4ce0372edc261893f9b650
malek <malekannabi5@gmail.com>
2021-01-31 16:01:36 +0100

    adding a delivery guy in the delivery by the admin

--- src/Controller/Admin/DeliveryCrudController.php
+++ src/Controller/Admin/DeliveryCrudController.php
@@ -4,21 +4,15 @@ namespace App\Controller\Admin;
 
 use App\Entity\Delivery;
 use App\Entity\DeliveryGuy;
-use App\Entity\User;
-use App\Form\UserRegistryType;
-use Cassandra\Type\UserType;
-use Doctrine\DBAL\Types\TimeImmutableType;
-use Doctrine\ORM\EntityRepository;
+
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
-use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+
 
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class DeliveryCrudController extends AbstractCrudController
 {
